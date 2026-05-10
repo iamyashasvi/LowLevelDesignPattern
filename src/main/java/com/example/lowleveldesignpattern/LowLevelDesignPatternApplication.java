fcbeb5011c67c1bf6d1a9bd188772bac3b30ef12
Yashasvi <iamyashasvi@gmail.com>
2026-05-10 16:58:43 +0530

    First Commit

---
+++ src/main/java/com/example/lowleveldesignpattern/LowLevelDesignPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.lowleveldesignpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LowLevelDesignPatternApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lowleveldesignpattern/LowLevelDesignPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lowleveldesignpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignPatternApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
